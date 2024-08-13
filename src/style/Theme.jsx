2817e671d4dfdbd9193054507e3074dfa977cfe2
holaleche <whoisleche@gmail.com>
2024-08-13 21:38:09 +0900

    Design: 전역 스타일 및 테마 설정

---
+++ src/style/Theme.jsx
@@ -0,0 +1,28 @@
+const fontSize = {
+  xxxsmall: "0.6rem",
+  xxsmall: "0.8rem",
+  xsmall: "1rem",
+  small: "1.2rem",
+  medium: "1.4rem",
+  large: "1.6rem",
+  xlarge: "1.8rem",
+  xxlarge: "2.4rem",
+};
+
+const color = {
+  backgroundColor: "#008081",
+  barColor: "#000080",
+  lightGrayColor: "#C0C0C0",
+  grayColor: "#A0A0A0",
+  darkGrayColor: "#808080",
+  whiteColor: "#FFFFFF",
+  blackColor: "#000000",
+  redColor: "#FD0101",
+};
+
+const Theme = {
+  fontSize,
+  color,
+};
+
+export default Theme;
