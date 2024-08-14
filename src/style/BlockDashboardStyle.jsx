53ae15cb58b9bea0626c36598662222aa52d0cc3
holaleche <whoisleche@gmail.com>
2024-08-14 15:19:52 +0900

    Design: Block Dashboard UI 구현

---
+++ src/style/BlockDashboardStyle.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const NextButtonContainer = styled.div`
+  position: absolute;
+  top: 5%;
+  right: 5%;
+`;
+
+export const LineBlockList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 15% 0;
+  gap: 2rem;
+`;
