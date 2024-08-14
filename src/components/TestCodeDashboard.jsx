9ad370d4576ddfc2632fd2f8a8231daf9741cc38
holaleche <whoisleche@gmail.com>
2024-08-14 15:20:37 +0900

    Design: Test Code Dashboard UI 구현

--- src/components/TestCodeDashboard.jsx
+++ src/components/TestCodeDashboard.jsx
@@ -1,3 +1,38 @@
-const TestCodeDashboard = () => {};
+import { useState, useEffect } from "react";
+
+import CodeBox from "./common/CodeBox";
+import Button from "./common/Button";
+
+import { Section, Header, Content } from "../style/CommonStyle";
+
+const TestCodeDashboard = ({ text }) => {
+  const [content, setContent] = useState(text);
+
+  const testCode = "TestCode";
+
+  useEffect(() => {
+    const interval = window.setInterval(() => {
+      setContent((content) =>
+        content === text + "..." ? text : content + ".",
+      );
+    }, 300);
+
+    return () => window.clearInterval(interval);
+  }, [text]);
+
+  return (
+    <Section>
+      <Header>
+        <h2>Test Code Dashboard</h2>
+      </Header>
+      <Content>
+        <h3 className="test-code-text">Result</h3>
+        <h3 className="test-code-text">{content}</h3>
+        <CodeBox testCode={testCode} />
+        <Button type="text" text="copy" />
+      </Content>
+    </Section>
+  );
+};
 
 export default TestCodeDashboard;
