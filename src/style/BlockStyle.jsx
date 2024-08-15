9698a8c9b78c9c296ca58d8f3caf005ef2e0445e
holaleche <whoisleche@gmail.com>
2024-08-15 22:54:42 +0900

    Design: Line Block 크기 및 배치 조정

--- src/style/BlockStyle.jsx
+++ src/style/BlockStyle.jsx
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
+const BlockListContainer = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const MethodBlockContainer = styled.li`
   .method-block-name {
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 1rem 1rem;
+    padding: 1rem 0;
+    height: 2rem;
     background: ${({ theme }) => theme.color.barColor};
     color: ${({ theme }) => theme.color.whiteColor};
     border: 0.15rem solid ${({ theme }) => theme.color.blackColor};
@@ -19,7 +26,8 @@ const InputBlockContainer = styled.li`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0.62rem 1rem;
+  height: 2rem;
+  padding: 1rem 0.8rem;
   background: ${({ theme }) => theme.color.grayColor};
   color: ${({ theme }) => theme.color.whiteColor};
   border: 0.15rem solid ${({ theme }) => theme.color.blackColor};
@@ -32,9 +40,9 @@ const InputBlockContainer = styled.li`
 const LineBlockContainer = styled.li`
   display: flex;
   align-items: center;
-  padding: 2rem 4rem;
+  padding: 2rem 1rem;
   padding-left: 0.5rem;
-  width: 16rem;
+  min-width: 20rem;
   background: ${({ theme }) => theme.color.darkGrayColor};
   color: ${({ theme }) => theme.color.blackColor};
   border-top: 0.1rem solid ${({ theme }) => theme.color.whiteColor};
@@ -43,4 +51,14 @@ const LineBlockContainer = styled.li`
   border-bottom: 0.15rem solid ${({ theme }) => theme.color.blackColor};
 `;
 
-export { MethodBlockContainer, InputBlockContainer, LineBlockContainer };
+const OrderNumber = styled.div`
+  margin-right: 0.5rem;
+`;
+
+export {
+  BlockListContainer,
+  MethodBlockContainer,
+  InputBlockContainer,
+  LineBlockContainer,
+  OrderNumber,
+};
