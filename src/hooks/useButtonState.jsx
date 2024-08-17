591b71d865ad7a1b0e55cca7aa384e5798d89cfa
holaleche <whoisleche@gmail.com>
2024-08-17 12:33:49 +0900

    Refactor: useButtonState Hook 최적화 (useEffect)

--- src/hooks/useButtonState.jsx
+++ src/hooks/useButtonState.jsx
@@ -11,21 +11,15 @@ const useButtonState = (lineBlocks) => {
     const hasBlock = lineBlocks.some(
       (lineBlock) => lineBlock.blocks.length > 0,
     );
-    setIsTextButtonDisabled((prevStates) => ({
-      ...prevStates,
-      reset: !hasBlock,
-    }));
-  }, [lineBlocks]);
-
-  useEffect(() => {
     const hasMethodBlock = lineBlocks.every((lineBlock) =>
       lineBlock.blocks.some((block) => block.type === "method"),
     );
-    setIsTextButtonDisabled((prevStates) => ({
-      ...prevStates,
+
+    setIsTextButtonDisabled({
+      reset: !hasBlock,
       next: !hasMethodBlock,
       create: !hasMethodBlock,
-    }));
+    });
   }, [lineBlocks]);
 
   return isTextButtonDisabled;
