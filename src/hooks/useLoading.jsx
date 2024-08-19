a8f65a0462905b8f37097ab72c69354b5a0e8215
holaleche <whoisleche@gmail.com>
2024-08-20 01:32:46 +0900

    Feat: Loading 관련 커스텀 훅 구현

---
+++ src/hooks/useLoading.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+import useStore from "../store";
+
+const useLoading = () => {
+  const [content, setContent] = useState("Loading");
+  const [isLoading, setIsLoading] = useState(false);
+  const [showCodeBox, setShowCodeBox] = useState(false);
+  const { testCodes } = useStore();
+
+  useEffect(() => {
+    if (testCodes) {
+      setIsLoading(true);
+
+      const loadingInterval = window.setInterval(() => {
+        setContent((currentContent) =>
+          currentContent === "Loading" + "..."
+            ? "Loading"
+            : currentContent + ".",
+        );
+      }, 200);
+
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+        setShowCodeBox(true);
+
+        clearInterval(loadingInterval);
+      }, 1000);
+
+      return () => {
+        clearInterval(loadingInterval);
+        clearTimeout(timer);
+      };
+    }
+  }, [testCodes]);
+
+  return {
+    content,
+    isLoading,
+    showCodeBox,
+    testCodes,
+  };
+};
+
+export default useLoading;
