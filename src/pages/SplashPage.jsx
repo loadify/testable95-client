10ab63c7315959f6179eb8a018ae929614cd28a1
holaleche <whoisleche@gmail.com>
2024-11-04 22:08:57 +0900

    Refactor: API 요청 주소 수정
    Feat: 진입 화면 구현

---
+++ src/pages/SplashPage.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+import Button from "../components/common/Button";
+
+import { SplashPageTitle, SplashPageContainer } from "../style/SplashPageStyle";
+
+const SplashPage = () => {
+  return (
+    <SplashPageContainer>
+      <SplashPageTitle>Testable 95</SplashPageTitle>
+      <Link to="/blocks" style={{ textDecoration: "none" }}>
+        <Button text="Start" />
+      </Link>
+    </SplashPageContainer>
+  );
+};
+
+export default SplashPage;
